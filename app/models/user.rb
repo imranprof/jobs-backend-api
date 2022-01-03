9981e66bc09291ea3062ac4b9de776aa5c163444
Kamrul Hassan <kh27.abir@gmail.com>
2022-01-03 22:21:31 +0600

    feat(User): create user with minimum fields for authentication

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+end
