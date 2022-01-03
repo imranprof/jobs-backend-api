8acde40653b34ec9fddbdbe445077dfb730704c0
Kamrul Hassan <kh27.abir@gmail.com>
2022-01-03 22:23:33 +0600

    feat(Authentication): authenticate a user based on their generated json web token

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request, only: :sign_in
+
+  def sign_in
+    command = AuthenticateUser.call(params[:email], params[:password])
+    if command.success?
+      render json: { user_email: command.result.email, auth_token: command.result.token }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+
+  def sign_out
+    command = current_user.update(token: '')
+    if command
+      render json: { sign_out: command }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+end
---
+++ app/controllers/concerns/authenticate_user.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'json_web_token'
+
+class AuthenticateUser
+  prepend SimpleCommand
+  attr_accessor :email, :password
+
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  def call
+    user = User.find_by(email: email)
+    if user&.authenticate(password)
+      api_key = Rails.application.credentials.seekrightjobs[:api_key]
+      auth_token = JsonWebToken.encode({ user_id: user.id, api_key: api_key })
+      user.update token: auth_token
+      user
+    else
+      errors.add :user_authentication, 'invalid credentials'
+      nil
+    end
+  end
+
+end
