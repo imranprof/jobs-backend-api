1b1fb23287fc3936a944c710d94de14a4794ac6f
Kamrul Hassan <kh27.abir@gmail.com>
2022-01-04 17:27:00 +0600

    fix: Use symbolic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@
 
 Rails.application.routes.draw do
   resources :users
-  post '/sign_in', to: 'authentication#sign_in'
-  delete '/sign_out', to: 'authentication#sign_out'
+  post :sign_in, to: 'authentication#sign_in'
+  delete :sign_out, to: 'authentication#sign_out'
 
   namespace :api do
     namespace :v1 do
-      get '/seek_right_jobs', to: 'api#seek_right_jobs'
+      resources :dashboard, only: [:index]
     end
   end
 end
