4d4b025e9b88004972c48a78bca92a5cca5e533b
Akash Das <aku.das02@gmail.com>
2022-05-17 21:13:03 +0600

    refactor: update the indentation in all migration files and remove extra line

--- db/migrate/20220516101425_create_education_histories.rb
+++ db/migrate/20220516101425_create_education_histories.rb
@@ -1,14 +1,14 @@
 class CreateEducationHistories < ActiveRecord::Migration[7.0]
   def change
     create_table :education_histories do |t|
-      t.string :institution, null: false
-      t.string :degree, null: false
-      t.string :grade, null: false
-      t.text :description, limit: 500, null: false
-      t.datetime :start_date, null: false
-      t.datetime :end_date, null: false
-      t.boolean :currently_enrolled, null: false
-      t.boolean :visibility, null: false
+      t.string     :institution, null: false
+      t.string     :degree, null: false
+      t.string     :grade, null: false
+      t.text       :description, limit: 500, null: false
+      t.datetime   :start_date, null: false
+      t.datetime   :end_date, null: false
+      t.boolean    :currently_enrolled, null: false
+      t.boolean    :visibility, null: false
       t.belongs_to :user, foreign_key: true, null: false
       t.timestamps
     end
