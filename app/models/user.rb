ee06757ffe2d31b32f44e7844c4266e25bd0d02c
adittorehan <adittorehanshaju@gmail.com>
2022-06-10 22:10:07 +0600

    refactor: add default features, blogs, and projects constants and new line at the end of file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,19 @@
 
 require 'factory_bot'
 
+FEATURES_ARRAY = ['business strategy', 'app development', 'app design', 'mobile app', 'CEO marketing',
+                  'UI & UX design'].freeze
+PROJECTS_ARRAY = [
+  { title: 'The services provide for design', filename: 'portfolio-01.jpg' },
+  { title: 'Mobile app landing design & app maintain', filename: 'portfolio-02.jpg' },
+  { title: 'Logo design creativity & application', filename: 'portfolio-03.jpg' }
+].freeze
+BLOGS_ARRAY = [
+  { title: 'The services provide for design', filename: 'blog-01.jpg' },
+  { title: 'Mobile app landing design & app maintain', filename: 'blog-02.jpg' },
+  { title: 'T-shirt design is the part of design', filename: 'blog-03.jpg' }
+].freeze
+
 class User < ApplicationRecord
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -26,37 +39,23 @@ class User < ApplicationRecord
 
   def default_user_profile
     FactoryBot.create(:user_profile, user: self)
-
     Skill.first(3).each do |skill|
       FactoryBot.create(:users_skill, user: self, skill: skill, rating: 90)
     end
-
-    ['business strategy', 'app development', 'app design', 'mobile app', 'CEO marketing', 'UI & UX design'].each do
-    |feature_title|
+    FEATURES_ARRAY.each do |feature_title|
       FactoryBot.create(:feature, user: self, title: feature_title)
     end
-
-    [
-      { title: 'The services provide for design', filename: 'portfolio-01.jpg' },
-      { title: 'Mobile app landing design & app maintain', filename: 'portfolio-02.jpg' },
-      { title: 'Logo design creativity & application', filename: 'portfolio-03.jpg' }
-    ].each do |project|
+    PROJECTS_ARRAY.each do |project|
       FactoryBot.create(:project, user: self, title: project[:title], filename: project[:filename])
     end
-
     FactoryBot.create(:education_history, user: self, institution: 'University of A', degree: 'diploma', grade: 'B',
-                      start_date: DateTime.iso8601('2016-01-01', Date::ENGLAND),
-                      end_date: DateTime.iso8601('2018-01-01', Date::ENGLAND))
-
-    FactoryBot.create(:work_history, user: self, title: 'The Personal Portfolio Mystery', employment_type: 0, company_name: 'Company A',
-                      start_date: DateTime.iso8601('2018-01-01', Date::ENGLAND),
-                      end_date: DateTime.iso8601('2020-01-01', Date::ENGLAND), currently_employed: true, visibility: true)
-
-    [
-      { title: 'The services provide for design', filename: 'blog-01.jpg' },
-      { title: 'Mobile app landing design & app maintain', filename: 'blog-02.jpg' },
-      { title: 'T-shirt design is the part of design', filename: 'blog-03.jpg' }
-    ].each do |blog|
+                                          start_date: DateTime.iso8601('2016-01-01', Date::ENGLAND),
+                                          end_date: DateTime.iso8601('2018-01-01', Date::ENGLAND))
+    FactoryBot.create(:work_history, user: self, title: 'The Personal Portfolio Mystery', employment_type: 0,
+                                     company_name: 'Company A', start_date: DateTime.iso8601('2018-01-01', Date::ENGLAND),
+                                     end_date: DateTime.iso8601('2020-01-01', Date::ENGLAND), currently_employed: true,
+                                     visibility: true)
+    BLOGS_ARRAY.each do |blog|
       FactoryBot.create(:blog, user: self, title: blog[:title], filename: blog[:filename])
     end
   end
