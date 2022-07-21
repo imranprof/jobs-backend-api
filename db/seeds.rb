c9190bb330382abf9e4c4ba9a3d2eae146da7518
adittorehan <adittorehanshaju@gmail.com>
2022-07-21 20:28:37 +0600

    fix: missing template issue of profile's show response

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ require 'factory_bot'
 skill_list = [
   { title: :Ruby, filename: 'ruby.png', icon_path: 'app/assets/images/skill/ruby.png' },
   { title: :Javascript, filename: 'javascript.png', icon_path: 'app/assets/images/skill/javascript.png' },
-  { title: :Python, filename: 'python.png', icon_path: 'app/assets/images/skill/python.png' }
+  { title: :Python, filename: 'python.png', icon_path: 'app/assets/images/skill/python.png' },
+  { title: :Golang, filename: 'golang.png', icon_path: 'app/assets/images/skill/golang.png' },
+  { title: :Java, filename: 'java.png', icon_path: 'app/assets/images/skill/java.png' }
 ]
 skill_list.each do |skill|
   FactoryBot.create(:skill, title: skill[:title], filename: skill[:filename])
