7c4176e6c3004701af9f5f1db69f5662e5bbccd1
Aditto <adittorehanshaju@gmail.com>
2022-08-22 20:51:54 +0600

    refactor: resolve the requested changes

--- app/controllers/api/v1p1/jobs_controller.rb
+++ app/controllers/api/v1p1/jobs_controller.rb
@@ -3,10 +3,9 @@
 module Api
   module V1p1
     class JobsController < ApplicationController
-      before_action :authenticate_request, only: %i[create update destroy]
-      before_action :set_job, only: %i[show update destroy]
-
-      EMPLOYER = 'employer'
+      prepend_before_action :authenticate_request, only: %i[create update destroy]
+      before_action :authenticate_job_request, only: %i[create update destroy]
+      before_action :set_job, only: %i[show]
 
       def index
         @jobs = Job.all
@@ -15,20 +14,17 @@ module Api
       def show; end
 
       def create
-        unless current_user.role == EMPLOYER
-          @error = 'Only employer can create job post'
-          render :error, status: :unprocessable_entity and return
-        end
         @job = current_user.jobs.new(jobs_params)
         unless @job.save
           @error = 'Unable to create job'
           render :error, status: :unprocessable_entity and return
         end
-        render :show, status: :ok
+        render :show, status: :created
       end
 
       def update
-        if @job.update(jobs_params)
+        @job = current_user.jobs.find_by(id: jobs_params[:id].to_i)
+        if @job&.update(jobs_params)
           render :show, status: :ok
         else
           @error = 'Failed to update job'
@@ -37,13 +33,23 @@ module Api
       end
 
       def destroy
-        @job.destroy
+        current_user.jobs.find_by(id: jobs_params[:id].to_i).destroy
+        head :no_content
+      end
+
+      def authenticate_job_request
+        @is_employer = current_user.role == 'employer'
+        return if params[:action] == 'create' && @is_employer
+        return if current_user.jobs.find_by(id: jobs_params[:id].to_i) && @is_employer
+
+        @error = 'You can not perform this action'
+        render :error, status: :unauthorized
       end
 
       private
 
       def jobs_params
-        params.permit(%i[id title description location skills])
+        params.require(:job).permit(%i[id title description location skills])
       end
 
       def set_job
