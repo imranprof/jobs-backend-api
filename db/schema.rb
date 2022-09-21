15905bf957d7738e34933db03255a5921600435b
imranprof <imran.ent320@gmail.com>
2022-09-21 20:19:03 +0600

    feat: new migration for add has_read column to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_123412) do
+ActiveRecord::Schema.define(version: 2022_09_21_141311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_123412) do
     t.integer "parent_message_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "has_read", default: false
   end
 
   create_table "projects", force: :cascade do |t|
