ae9e5bf65ac97f6f4efe1e539cafcb2f86e3faba
imranprof <imran.ent320@gmail.com>
2022-10-12 19:04:48 +0600

    feat: add job offers show api and single job offer show api

--- app/controllers/api/v1p1/job_applications_controller.rb
+++ app/controllers/api/v1p1/job_applications_controller.rb
@@ -3,11 +3,30 @@
 module Api
   module V1p1
     class JobApplicationsController < ApplicationController
-      prepend_before_action :authenticate_request, only: %i[show_job_offers]
+      prepend_before_action :authenticate_request, only: %i[job_offers show_job_offer]
+      before_action :authenticate_application_request, only: %i[job_offers show_job_offer]
 
-      def show_job_offers
-        @job_offer = current_user.job_applications.where('hire = ?', true)
+      def job_offers
+        @job_offers = current_user.job_applications.where('hire = ?', true)
       end
+
+      def show_job_offer
+        @job_offer = current_user.job_applications.find_by(id: params[:id])
+        return if @job_offer
+
+        @error = 'Job offer not found'
+        render :error, status: :not_found
+      end
+
+      def authenticate_application_request
+        @is_employer = current_user.role == 'employer'
+        return if params[:action] == 'job_offers' && !@is_employer
+        return if params[:action] == 'show_job_offer' && !@is_employer
+
+        @error = 'You can not perform this action'
+        render :error, status: :unauthorized
+      end
+
     end
   end
 end
---
+++ app/views/api/v1p1/job_applications/show_job_offer.jbuilder
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+json.offer_details do
+  json.id @job_offer.id
+  json.title @job_offer.job.title
+  json.hire_rate @job_offer.hire_rate[0]
+end
