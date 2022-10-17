6c85f95884f3a6a9b12ebe99fecdbc5e93d24bc1
imranprof <imran.ent320@gmail.com>
2022-10-17 22:04:28 +0600

    feat: add enum for job status

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Job < ApplicationRecord
+  enum status: { draft: 0, published: 1, closed: 2, canceled: 3 }
+
   belongs_to :employer, foreign_key: :user_id, class_name: 'User'
   has_many :job_applications, dependent: :destroy
   has_many :applicants, through: :job_applications, source: :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_131513) do
+ActiveRecord::Schema.define(version: 2022_10_17_151916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(version: 2022_10_12_131513) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "pay_type"
     t.integer "budget", default: [], array: true
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
