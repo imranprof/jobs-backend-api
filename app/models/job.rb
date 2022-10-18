5e93a9183082e718c94910b7e7c1c089c149cdf2
imranprof <imran.ent320@gmail.com>
2022-10-18 18:26:58 +0600

    refactor: modify job status enum hash key name

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Job < ApplicationRecord
-  enum status: { draft: 0, published: 1, closed: 2, canceled: 3 }
+  enum status: { Draft: 0, Published: 1, Closed: 2, Canceled: 3 }
 
   belongs_to :employer, foreign_key: :user_id, class_name: 'User'
   has_many :job_applications, dependent: :destroy
