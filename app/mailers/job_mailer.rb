28b744fe62c4de2124763ceeaf04d5cac6ae2401
imranprof <imran.ent320@gmail.com>
2022-11-01 16:53:13 +0600

    feat: add mail notification for job contract feedback

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -14,4 +14,11 @@ class JobMailer < ApplicationMailer
     mail(to: @recipient.email, subject: "You have a contract end notification  on #{@contract.job.title} job")
   end
 
+  def contract_feedback_rating_notification_mail(contract, modifier, recipient)
+    @contract = contract
+    @modifier = modifier
+    @recipient = recipient
+    mail(to: @recipient.email, subject: "You have a feedback  on #{@contract.job.title} job")
+  end
+
 end
