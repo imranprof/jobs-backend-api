2f45967eb6c2676a73f4045bdcbf6886af5f75cb
imranprof <imran.ent320@gmail.com>
2022-11-08 20:51:33 +0600

    feat: add action for create new time sheet for a job contract

--- app/controllers/api/v1p1/job_contracts_controller.rb
+++ app/controllers/api/v1p1/job_contracts_controller.rb
@@ -4,6 +4,26 @@ module Api
   module V1p1
     class JobContractsController < ApplicationController
 
+      before_action :authenticate_request, only: %i[add_working_details]
+
+      def add_working_details
+        contract_id = working_details_param[:job_application_id]
+        @contract = current_user.job_applications&.find_by(id: contract_id)
+        @time_sheet = @contract&.time_sheets&.new(working_details_param)
+        unless @time_sheet&.save
+          @error = 'Unable to create time sheet for job contract'
+          render :error, status: :unprocessable_entity and return
+        end
+        head :created
+      end
+
+      private
+
+      def working_details_param
+        params.require(:job_contract).permit(%i[id start_date end_date work_description work_hours job_application_id])
+
+      end
+
 
     end
   end
