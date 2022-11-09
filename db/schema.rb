121158a7e3481ce5aa5ca6dd58e409ce6038e96d
imranprof <imran.ent320@gmail.com>
2022-11-09 20:55:50 +0600

    refactor: solve work hours fraction problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_144512) do
+ActiveRecord::Schema.define(version: 2022_11_09_145327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -207,7 +207,7 @@ ActiveRecord::Schema.define(version: 2022_11_09_144512) do
     t.date "start_date"
     t.date "end_date"
     t.text "work_description"
-    t.decimal "work_hours", precision: 3, scale: 2
+    t.decimal "work_hours", precision: 4, scale: 2
     t.bigint "job_application_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
